1e8cdcbe52ba2f4b7add632bb5ec0fe646686b5d
Zhiyu Lei <zlei6@seas.upenn.edu>
2023-03-04 06:20:15 -0500

    PreProcessing and matrix transform

--- PreProcessing/Split_Normalize.py
+++ PreProcessing/Split_Normalize.py
@@ -1,57 +1,62 @@
-import pandas as pd
 import numpy as np
-train = pd.read_csv("/Users/xinchengzhu/Downloads/train.csv")
+import pandas as pd
 
-#filter missing data and useless columns
-train = train[train['MISSING_DATA'] == False] 
-train = train[train['POLYLINE'].map(len) > 1] #see line 10
-train = train[['POLYLINE']]
 
-#choose first 10000 rows from dataset to run without first row(data type trouble)
-train_1 = train.head(10000)
-train_1 = train_1[train_1['POLYLINE'] != '[]']
+def _change_type(polyline):
+    polyline = polyline[2:-2]
+    cords_raw = polyline.split("],[")
+    cords = []
+    max_lon, min_lon, max_lat, min_lat = float("-inf"), float("inf"), float("-inf"), float("inf")
+    for cord in cords_raw:
+        cords.append([float(loc) for loc in cord.split(",")])
+        max_lon = max(max_lon, cords[-1][0])
+        min_lon = min(min_lon, cords[-1][0])
+        max_lat = max(max_lat, cords[-1][1])
+        min_lat = min(min_lat, cords[-1][1])
+    return pd.Series({"POLYLINE": cords, "max_lon": max_lon, "min_lon": min_lon, "max_lat": max_lat, "min_lat": min_lat})
 
-#Change type
-for i in range(len(train_1)):
-    a = train_1['POLYLINE'].iloc[i]
-    a = a[2:-2]
-    b = a.split('],[')
-    new = []
-    for item in b:
-        new.append(item.split(','))
-    for k in range(len(new)):
-        new[k] = [float(j) for j in new[k]]
-    a = new
 
-# Normalize MinMax
-max_latitude = train_1["POLYLINE"].iloc(0)[0][0][0]
-min_latitude = train_1["POLYLINE"].iloc(0)[0][0][0]
-max_longtitude = train_1["POLYLINE"].iloc(0)[0][0][1]
-min_longtitude = train_1["POLYLINE"].iloc(0)[0][0][1]
-for i in range(len(train_1)):
-    for cord in train_1["POLYLINE"].iloc(0)[i]:
-        max_latitude = max(max_latitude,cord[0])
-        max_longtitude = max(max_longtitude,cord[1])
-        min_latitude = min(max_latitude,cord[0])
-        min_longtitude = min(max_longtitude,cord[1])
-print(max_longtitude,max_latitude,min_latitude,min_longtitude)
+def _normalize(polyline, max_lon, min_lon, max_lat, min_lat):
+    final = [[(cord[0]-min_lon)/(max_lon-min_lon), (cord[1] - min_lat) / (max_lat - min_lat)] for cord in polyline]
+    return pd.Series({"POLYLINE_INIT": final[:-1], "POLYLINE_DEST": final[-1]})
 
-for i in range(len(train_1)):
-    final = []
-    for cord in train_1["POLYLINE"].iloc[i]:
-        temp = []
-        temp.append((cord[0]-min_latitude)/(max_latitude-min_latitude))
-        temp.append((cord[1]-min_longtitude)/(max_longtitude-min_longtitude))
-        final.append(temp)
-    train_1["POLYLINE"].iloc[i] = final
 
-# Split
-train_1["POLYLINE_INIT"] = train_1["POLYLINE"]
-train_1["POLYLINE_DEST"] = train_1["POLYLINE"]
-for i in range(len(train_1)):
-    train_1["POLYLINE_INIT"].iloc[i] = train_1["POLYLINE"].iloc[i][:-1]
-    train_1["POLYLINE_DEST"].iloc[i] = train_1["POLYLINE"].iloc[i][-1]
-train_1_clean = train_1[['POLYLINE_INIT','POLYLINE_DEST']]
+def _to_matrix(polyline, m):
+    mat = np.zeros((m, m))
+    n = len(polyline)
+    for i in range(n):
+        x = min(m - 1, int(polyline[i][0] * m))
+        y = min(m - 1, int(polyline[i][1] * m))
+        mat[y][x] = (i + 1) / n
+    return mat
 
-#Save to CSV
-train_1_clean.to_csv('/Users/xinchengzhu/Downloads/train_1_clean.csv')  
+
+def transform(df_train, m):
+    # Change type
+    changed = df_train["POLYLINE"].apply(_change_type)
+    df_train["POLYLINE"] = changed["POLYLINE"]
+    # Get min-max
+    max_longitude = changed["max_lon"].max()
+    min_longitude = changed["min_lon"].min()
+    max_latitude = changed["max_lat"].max()
+    min_latitude = changed["min_lat"].min()
+    # Normalize min-max and split
+    cleaned = train_1["POLYLINE"].apply(_normalize, args=(max_longitude, min_longitude, max_latitude, min_latitude))
+    # Transform to matrices
+    cleaned["MATRIX"] = cleaned["POLYLINE_INIT"].apply(_to_matrix, args=(m,))
+    return cleaned
+
+
+if __name__ == "__main__":
+    train = pd.read_csv("../data/train.csv")
+    # Filter missing data and useless columns
+    train = train[train["MISSING_DATA"] == False]
+    train = train[train["POLYLINE"].map(len) > 1]
+    train = train[train["POLYLINE"] != "[]"]
+    train = train[["POLYLINE"]]
+    # Choose 10000 rows randomly from dataset to run
+    train_1 = train.sample(10000)
+
+    transformed = transform(train_1, 256)
+    # Save to CSV
+    transformed.to_csv("../data/train_transformed.csv")
